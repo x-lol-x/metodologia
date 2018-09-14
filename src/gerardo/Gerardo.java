9455158c942777eb8578a9a0dedf551faae9a91c
T-101 <T-101@PC218>
2018-09-13 19:45:14 -0500

    mi primer proyecto de github

---
+++ src/gerardo/Gerardo.java
@@ -0,0 +1,15 @@
+package gerardo;
+
+import javax.swing.JOptionPane;
+public class Gerardo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // System.out.println("Hola mundo");
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+       
+    }
+    
+}
